93e52e6ad50698c16070f37242139970eb7d6753
u31d74 <u31d74@INGURWN110444>
2023-11-02 15:51:58 +0530

    Add project files.

---
+++ WebApplicationDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationDemo.Models;
+
+namespace WebApplicationDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        List<Employee> employees = null;
+        public EmployeeController() {
+
+            if (employees == null)
+            {
+                employees = new List<Employee>()
+                {
+                    new Employee() { Id = 1, Name = "Ajay", Dept = "hr", Salary = 150 },
+
+                    new Employee() { Id = 2, Name = "Rakhi", Dept = "CSE", Salary = 500 },
+
+                    new Employee() { Id = 3, Name = "Nupoor", Dept = "Manager", Salary = 300 },
+
+                    new Employee() { Id = 4, Name = "Avantika", Dept = "hr", Salary = 150 }
+                };
+            }
+        }
+       
+        public IActionResult Index()
+        {
+            
+           
+            return View(employees.ToList());
+        }
+    }
+}
---
+++ WebApplicationDemo/Controllers/FirstController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationDemo.Controllers
+{
+    public class FirstController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index1()
+        {
+            return View();
+        }
+    }
+}
